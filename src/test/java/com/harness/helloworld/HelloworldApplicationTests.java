3971f55a029af07461cf506e7a08c880ac025d35
DIEGO AMPUERO CUTTY <99100371@iecisa.corp>
2022-08-10 17:59:23 -0500

    first load

---
+++ src/test/java/com/harness/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harness.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
